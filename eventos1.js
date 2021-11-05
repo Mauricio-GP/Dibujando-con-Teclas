229af89f13ef1e32d003a2ca93a5117cd9f3cb31
Mauricio-GP <anmaoguepa@gmail.com>
2021-11-05 16:36:36 -0500

    segundo commit

--- eventos1.js
+++ eventos1.js
@@ -13,24 +13,6 @@ var papel = cuadrito.getContext("2d");
 var x = 150;
 var y = 150;
 
-function marco()
-{
-    var lineas = parseInt(texto.value);
-    var l = 0;
-    var yi, xf;
-    var colorcito = "#FAA";
-    var espacio = ancho / lineas;
-  
-    for(l = 0; l < lineas; l++)
-    {
-      yi = espacio * l;
-      xf = espacio * (l + 1);
-      dibujarLinea(colorcito, 0, yi, xf, 300);
-      console.log("linea " + 1);
-    }
-
-  
-}
 
 dibujarLinea(colorcito, 1,1,1,299);
 dibujarLinea(colorcito, 1,299,299,299);
@@ -75,3 +57,4 @@ function dibujarTeclado(evento)
         break;
     }
 }
+console.log("prueba");
